f9f7deee71d580d1db5b8d1c3457a751836fa265
leeuihyun <meta212121@naver.com>
2025-07-09 00:35:38 +0900

    chore : JVM ConcurrentHashMap Cache 기능 설정

---
+++ src/main/java/com/example/mall/global/config/CacheConfig.java
@@ -0,0 +1,10 @@
+package com.example.mall.global.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+}
